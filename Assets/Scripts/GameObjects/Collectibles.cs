0fd05af0c1e2414a58dc3dee36214f5108af0952
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-01-30 14:42:00 -0600

    feat - Collectibles with spatial audio

---
+++ Assets/Scripts/GameObjects/Collectibles.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    public float amplitude;
+    public float frequency;
+
+    private Vector3 startingPosition;
+    private Vector3 tempPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempPosition = startingPosition;
+        tempPosition.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        transform.position = tempPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Collected!");
+            //FindObjectOfType<AudioManager>().PlaySound("Temp Collectible");
+            Destroy(gameObject);
+        }
+    }
+}
