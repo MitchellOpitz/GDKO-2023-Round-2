af03bfd0b9170d0eba3d850c7e9cb23f4aebd26d
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-01-30 22:02:57 -0600

    feat - Colored platforms unlock

---
+++ Assets/Scripts/GameObjects/Activation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activation : MonoBehaviour
+{
+    public Color purple = new Color((float)224 / 255, 0, (float)225 / 255, (float)255 / 255);
+    public Color blue = new Color(0, (float)133 / 255, (float)255 / 255, (float)255 / 255);
+    public Color green = new Color(0, (float)255 / 255, (float)38 / 255, (float)255 / 255);
+    public Color yellow = new Color((float)255 / 255, (float)238 / 255, 0, (float)255 / 255);
+    public Color orange = new Color((float)255 / 255, (float)108 / 255, 0, (float)255 / 255);
+    public Color red = new Color((float)255 / 255, 0, 0, (float)255 / 255);
+    public Color pink = new Color((float)255 / 255, 0, (float)164 / 255, (float)255 / 255);
+
+    public Color thisColor;
+
+    public void Activate()
+    {
+        this.GetComponent<SpriteRenderer>().color = thisColor;
+    }
+}
--- Assets/Scripts/GameObjects/Collectibles.cs
+++ Assets/Scripts/GameObjects/Collectibles.cs
@@ -7,6 +7,7 @@ public class Collectibles : MonoBehaviour
     public float amplitude;
     public float frequency;
     public GameObject nextCollectible;
+    public string colorUnlocked;
 
     private Vector3 startingPosition;
     private Vector3 tempPosition;
@@ -28,11 +29,20 @@ public class Collectibles : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
-        {
-            Debug.Log("Collected!");
+        {            
             //FindObjectOfType<AudioManager>().PlaySound("Temp Collectible");
             nextCollectible.SetActive(true);
+            ColorUnlock();
             Destroy(gameObject);
+        }
+    }
+
+    private void ColorUnlock()
+    {
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag(colorUnlocked);
+        foreach (GameObject obstacle in obstacles)
+        {
+            obstacle.GetComponent<Activation>().Activate();
         }
     }
 }
